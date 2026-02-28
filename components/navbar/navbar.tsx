a17ebd7bdace13a26424651254c1f6d174216392
Nathan Mota <nathansmota@gmail.com>
2026-02-27 23:19:29 -0300

    feat: style link navbar indication

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
         <div className="max-w-3xl mx-auto space-y-8 py-2 px-4 lg:px-0">
             <nav className="flex items-center justify-between py-4">
                 {isHome ? (
-                    <div className="flex items-center font-display font-bold text-lg">
+                    <div className="flex items-center font-display font-bold text-base md:text-lg">
                         Nathan Mota
                     </div>
                 ) : (
@@ -24,11 +24,30 @@ export default function Navbar() {
                         </Link>
                     </Button>
                 )}
-                <div className="flex items-center gap-4 md:gap-6">
-                    <Link href="/projects" className="nav-link">/projects</Link>
-                    <Link href="/articles" className="nav-link">/articles</Link>
-                    <Link href="/contact" className="nav-link">/contact</Link>
-                    <ThemeToggle />
+                <div className="flex items-center lg:gap-4 md:gap-6">
+                    <Link
+                        href="/projects"
+                        className={`nav-link ${pathname === "/projects" ? "active" : ""}`}
+                    >
+                        /projects
+                    </Link>
+
+                    <Link
+                        href="/articles"
+                        className={`nav-link ${pathname === "/articles" ? "active" : ""}`}
+                    >
+                        /articles
+                    </Link>
+
+                    <Link
+                        href="/contact"
+                        className={`nav-link ${pathname === "/contact" ? "active" : ""}`}
+                    >
+                        /contact
+                    </Link>
+                    <div className="ml-2 sm:ml-0 flex justify-center">
+                        <ThemeToggle />
+                    </div>
                 </div>
             </nav>
         </div>
